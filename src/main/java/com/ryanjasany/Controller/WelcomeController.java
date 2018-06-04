fa88ef04320549ad01d6dacfe9e0c328bfe1a592
ryanjasany <ryanjasany6@gmail.com>
2018-06-04 10:38:53 -0400

    build student catalog

--- src/main/java/com/ryanjasany/Controller/WelcomeController.java
+++ src/main/java/com/ryanjasany/Controller/WelcomeController.java
@@ -2,12 +2,20 @@ package com.ryanjasany.Controller;
 
 import java.util.Map;
 
+import com.ryanjasany.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WelcomeController {
+
+
+    @Autowired
+    private StudentService studentService;
 
     // inject via application.properties
     @Value("${welcome.message:test}")
@@ -21,7 +29,20 @@ public class WelcomeController {
 
     @RequestMapping("/cat")
     public String cat(Map<String, Object> model) {
-        model.put("message", this.message);
+
+
+        String name = studentService.getStudentById(1).getName();
+
+        model.put("message", name);
+        model.put("student", studentService.getStudentById(1));
+        return "cat";
+    }
+
+    @RequestMapping(value = "/cat/{id}", method = RequestMethod.GET)
+    public String number(Map<String, Object> model,  @PathVariable("id") int id){
+
+
+        model.put("student", studentService.getStudentById(id));
         return "cat";
     }
 }
